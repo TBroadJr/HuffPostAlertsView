e0845e3340c7f4bdc619c10d3099efc6825bed75
Tornelius <tkbjr28@icloud.com>
2023-01-22 11:49:48 -0500

    Initial Commit

---
+++ HuffPostAlertsView/HuffPostAlertsViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuffPostAlertsViewApp.swift
+//  HuffPostAlertsView
+//
+//  Created by Tornelius Broadwater, Jr on 1/22/23.
+//
+
+import SwiftUI
+
+@main
+struct HuffPostAlertsViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
